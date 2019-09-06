fef2aa33c2e8cb05c3e6e077f0599549f321715f
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-09-06 07:29:20 +0700

    initial-commit

---
+++ gridsome.config.js
@@ -0,0 +1,37 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: "Giftbox Promosindo",
+  siteUrl: "https://idnesdotlink.github.io",
+  pathPrefix: "giftbox.co.id",
+  siteDescription: "Giftbox Promosindo",
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/posts/**/*.md",
+        typeName: "Post",
+        route: "/blog/:slug",
+      },
+    },
+  ],
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      plugins: ["@gridsome/remark-prismjs"],
+    },
+  },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  }
+};
