8766273460b83ae69fc26b5220494b1881d13319
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-09-13 17:20:43 +0700

    ok

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -43,10 +43,11 @@ body {
 }
 
 .layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
 }
 
 .header {
