4f0886d821d46e79dc9c978349a371ff90e84379
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-09-16 06:46:02 +0700

    content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,23 @@
 <template>
   <Layout>
     <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>As the major player in its market, GIFTBOX PROMOSINDO as proven since 2006, shows consistent and progressive expansion in promoting, marketing and customizing the high-quality premium souvenirs and gifts.</p>
+
+    <p>With the reputation is our benchmark, GIFTBOX PROMOSINDO has served corporate, business-level to individual's needs with excellent designs, great products, on-time delivery and innovation. This ensures our accountability in all aspects for customers when dealing with us.</p>
+
+    <p>Giftbox Promosindo is also managed its own printing service, called Surapati Printing Center. With the all-in house production equipment and less outsourcing, giftBOX is able to deliver the best product with competitive price in the promotional market, decrease promotion cost with the best outcome to maximize the corporate promotion effectiveness.</p>
+
+    <p>Customers can walk into our showroom in Jl. BKR no. 145 to view an extensive range of product samples and discuss with our in-house sales executive with regards to their requirements and specifications.</p>
+
+    <p>Visit Us and Get a Good Deal !</p>
+
+    <p>Regards,</p>
+
+    <p>Rendy Renaldi</p>
+
+    <p>Director</p>
+
+    <p>CV. GIFTBOX PROMOSINDO</p>
   </Layout>
 </template>
 
---
+++ src/pages/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <h1>Contact</h1>
+    <h2>OFFICE &amp; SHOWROOM</h2>
+    <address>
+      <p>Jl. BKR no. 145 Bandung | West Java - Indonesia</p>
+      <p>Ph. / Fax. +62 22 520 8772</p>
+      <p>rendy.giftbox@yahoo.com</p>
+      <p>Direct Line/WA: +62818216820</p>
+    </address>
+
+    <h2>SPC DIGITAL PRINTING</h2>
+    <address>
+      <p>Jl. BKR no. 145 Bandung | West Java - Indonesia</p>
+      <p>Ph. / Fax. +62 22 520 8772</p>
+      <p>surapatiprinting@yahoo.com</p>
+      <p>www.surapatiprinting.com</p>
+    </address>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contact'
+  }
+}
+</script>
