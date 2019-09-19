99846b9291dc42f24db02ac44c67e8660823d6ff
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-09-20 05:25:21 +0700

    ok

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,25 +5,7 @@
     </ClientOnly>
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
 
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <PostList
-      v-for="edge in $page.allPost.edges"
-      :key="edge.node.id"
-      :post="edge.node"
-    />
-
-    <p class="home-links">
-      <a
-        href="https://gridsome.org/docs"
-        target="_blank"
-        rel="noopener"
-      >Gridsome Docs</a>
-    </p>
+    <About />
 
   </Layout>
 </template>
@@ -31,10 +13,12 @@
 <script>
 import PostList from "@/components/PostList"
 import Slide from "@/components/Slide"
+import About from "@/components/About"
 export default {
   components: {
     PostList,
-    Slide
+    Slide,
+    About
   },
   metaInfo: {
     title: 'Hello, world!'
