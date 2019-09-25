9c2f8c0a81eb478f20fd18638c0b148c72e29283
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-09-25 12:13:24 +0700

    Updates

---
+++ assets/js/page--src-pages-catalogs-vue.c5fd346d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{346:function(t,n,e){"use strict";e.r(n);var s=e(15),a=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Layout",[this._v("\n  Catalog List\n")])}),[],!1,null,null,null);n.default=a.exports}}]);
