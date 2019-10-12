4bc0d08ee1e244a4816a152312feec1d6f80ce8e
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-10-12 19:36:23 +0700

    ok

--- src/components/Coba.vue
+++ src/components/Coba.vue
@@ -1,11 +1,16 @@
 <template>
-
-  <agile>
+  <agile
+    class="test"
+    :slides-to-show="3"
+  >
     <div
       v-for="product in products"
       :key="product.node.id"
     >{{ product.node.title }}
-      <img :src="product.node.image.src" />
+      <img
+        :src="product.node.image.src"
+        class="h-auto w-25"
+      />
     </div>
   </agile>
 
@@ -21,3 +26,23 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.test.agile {
+  .agile__actions {
+    .agile__dots {
+      .agile__dot {
+        @apply mx-1;
+        button {
+          @apply border-giftbox border-2 border-solid w-4 h-4 rounded-full;
+        }
+      }
+      .agile__dot--current {
+        button {
+          @apply border-giftbox border-2 border-accent rounded-full;
+        }
+      }
+    }
+  }
+}
+</style>
