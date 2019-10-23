baf3bfec550eba7b0142bd36ff1cffaeca72c725
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-10-23 12:04:06 +0700

    4

--- src/layouts/Layout-4.vue
+++ src/layouts/Layout-4.vue
@@ -1,11 +1,21 @@
 <template>
-  <div :class="$name">
+  <div :class="'layout-4'">
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'layout-4'
+  name: 'layout-4',
+  data () {
+    return {
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.layout-4 {
+  @apply w-screen h-screen flex flex-col;
+}
+</style>
--- src/pages/Home-4.vue
+++ src/pages/Home-4.vue
@@ -1,13 +1,46 @@
 <template>
-  <Layout>home 4</Layout>
+  <Layout>
+    <header class="">
+      <div>Title</div>
+      <div v-waypoint="{ active: true, callback: onWaypoint, options: intersectionOptions }">nav</div>
+    </header>
+    <div class="main-content">
+      <div style="height: 600px;">
+        content
+      </div>
+    </div>
+    <footer>Footer</footer>
+  </Layout>
 </template>
 
 <script>
 import Layout from '@/layouts/Layout-4'
 export default {
+  data () {
+    return {
+      intersectionOptions: {
+
+      }
+    }
+  },
   components: {
     Layout
+  },
+  methods: {
+    onWaypoint ({ going, direction }) {
+      if (going === 'out' && direction === 'top') {
+        console.log('out to top')
+      }
+      if (going === 'in' && direction === 'bottom') {
+        console.log('in to bottom')
+      }
+    }
   }
-  
 }
 </script>
+
+<style lang="scss">
+.main-content {
+  @apply flex-1;
+}
+</style>
