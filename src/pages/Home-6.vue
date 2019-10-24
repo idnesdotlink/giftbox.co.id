492476411e1ca52696b695bf6e19989383760c0b
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-10-24 12:57:14 +0700

    ok

--- src/pages/Home-6.vue
+++ src/pages/Home-6.vue
@@ -80,48 +80,50 @@ export default {
 }
 </script>
 
-<style>
-.c {
-  margin: 0 auto;
-  padding: 16px;
-}
-nav {
-  display: flex;
-  width: 100vw;
-  margin: 0;
-  padding: 0;
-  background: #fff;
-  border-bottom: solid 1px #e5e5e5;
-}
-nav.vue-fixed-header--isFixed {
-  position: fixed;
-  left: 0;
-  top: 0;
-  z-index: 1000;
-  opacity: 0;
-  animation: fadeIn 0.3s ease-out forwards;
-}
-
-@keyframes fadeIn {
-  0% {
+<style lang="scss">
+.home-6 {
+  .c {
+    margin: 0 auto;
+    padding: 16px;
+  }
+  nav {
+    display: flex;
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+    background: #fff;
+    border-bottom: solid 1px #e5e5e5;
+  }
+  nav.vue-fixed-header--isFixed {
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 1000;
     opacity: 0;
-    transform: translateY(-74px);
+    animation: fadeIn 0.3s ease-out forwards;
   }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(-74px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
   }
-}
-.c.headerIsFixed {
-  transform: translateY(56px) !important;
-}
-.fixed-footer {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-}
-body.home-6 #app {
-  overflow: hidden;
+  .c.headerIsFixed {
+    transform: translateY(56px) !important;
+  }
+  .fixed-footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+  }
+  #app {
+    overflow: hidden;
+  }
 }
 </style> 
