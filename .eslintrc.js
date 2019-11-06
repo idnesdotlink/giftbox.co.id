72ca3bcb0c4cb822eff5a75f767f67bd9e4c5070
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-11-06 07:26:57 +0700

    var

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,17 @@
 module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+    // specifying a module sourcetype prevent eslint from marking import statements as errors
+    sourceType: "module"
+  },
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
+    "eslint:recommended",
     "plugin:vue/recommended"
   ],
   rules: {
